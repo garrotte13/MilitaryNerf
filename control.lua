1b2f0df87c755d3d1b8f033914cb3a5b4af7fb07
Alexander <95149904+garrotte13@users.noreply.github.com>
2023-04-24 13:51:15 +0300

    fixed chunk visibility nests expansion detect

--- control.lua
+++ control.lua
@@ -2,7 +2,7 @@
 script.on_event(defines.events.on_biter_base_built, function(event)
     local entity = event.entity
     if entity.valid and entity.type == "unit-spawner" then
-      local chunk_position = {x = math.ceil(entity.position.x/32), y = math.ceil(entity.position.y/32)}
+      local chunk_position = {x = math.floor(entity.position.x/32), y = math.floor(entity.position.y/32)}
       if true  then -- distance(global.benn, built_base.position) > 1 
         for _, player in pairs(game.connected_players) do
           --local player_settings = settings.get_player_settings(player)
